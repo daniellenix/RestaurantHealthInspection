0843b5f450ca01e50341693fdb169dc8b3c7faef
Haoyi An <haoyia@sfu.ca>
2020-03-02 23:29:44 -0800

    Add logic for RestaurantListActivity.

--- app/src/main/java/ca/sfu/prjCalcium/pr1/UI/RestaurantDetailActivity.java
+++ app/src/main/java/ca/sfu/prjCalcium/pr1/UI/RestaurantDetailActivity.java
@@ -2,11 +2,23 @@ package ca.sfu.prjCalcium.pr1.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import ca.sfu.prjCalcium.pr1.R;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+
+    public static final String RESTAURANT_POSITION_PASSED_IN = "restaurant_position_passed_in";
+
+    public static Intent makeIntent(Context c, int restaurantPosition) {
+        Intent intent = new Intent(c, RestaurantDetailActivity.class);
+
+        intent.putExtra(RESTAURANT_POSITION_PASSED_IN, restaurantPosition);
+        
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ca/sfu/prjCalcium/pr1/UI/RestaurantListActivity.java
+++ app/src/main/java/ca/sfu/prjCalcium/pr1/UI/RestaurantListActivity.java
@@ -1,16 +1,81 @@
 package ca.sfu.prjCalcium.pr1.UI;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.RestrictionsManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import ca.sfu.prjCalcium.pr1.Model.Restaurant;
+import ca.sfu.prjCalcium.pr1.Model.RestaurantManager;
 import ca.sfu.prjCalcium.pr1.R;
 
 public class RestaurantListActivity extends AppCompatActivity {
+
+    public static Intent makeIntent(Context c) {
+        return new Intent(c, RestaurantListActivity.class);
+    }
+
+//    placeholder code for getting the list of restaurants
+//    RestaurantManager rManager = RestaurantManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        populateRestaurantList();
     }
+
+    private void populateRestaurantList() {
+//         TODO: This is a placeholder function for now, since right now we don't have the names of the views
+//         this block of the code is pretty much just gonna be the logic, names can be easily changed
+//         after the fact
+//
+//         Might need a custom ListAdapter to show all information required.
+//        ArrayAdapter<Restaurant> restaurantArrayAdapter = new MyRestaurantAdapter();
+//
+//        ListView listRestaurant = findViewById(R.id.restaurantListActivityMainList);
+//        // listRestaurant.setAdapter();
+//        listRestaurant.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent rDetailIntent = RestaurantDetailActivity.makeIntent(RestaurantListActivity.this, position);
+//                startActivity(rDetailIntent);
+//            }
+//        });
+    }
+
+//    private class MyRestaurantAdapter extends ArrayAdapter<Restaurant> {
+//
+//        public MyRestaurantAdapter() {
+//            // RManager should be able to output as a list for the ArrayAdapter to work with
+//            super(RestaurantListActivity.this, R.layout.restaurant_item, rManager.listify());
+//        }
+//
+//        @NonNull
+//        @Override
+//        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//            View itemView = convertView;
+//
+//            if (itemView == null) {
+//                itemView = getLayoutInflater().inflate(R.layout.restaurant_item, parent, false);
+//            }
+//
+//            // Find the Restaurant to work with
+//            Restaurant r = rManager.getRestaurantByPosition(position);
+//
+//            // TODO: Fill the View
+//
+//            return itemView;
+//        }
+//    }
 }
