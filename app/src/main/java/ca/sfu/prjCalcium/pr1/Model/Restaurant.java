21015c1c4755a0550b490f0dc6d4ccf708e1cd08
Haoyi An <haoyia@sfu.ca>
2020-03-05 21:57:43 -0800

    RestaurantManager is now singleton.

--- app/src/main/java/ca/sfu/prjCalcium/pr1/Model/Restaurant.java
+++ app/src/main/java/ca/sfu/prjCalcium/pr1/Model/Restaurant.java
@@ -8,7 +8,7 @@ public class Restaurant {
     private String facType;
     private double latitude;
     private double longitude;
-    //InspectionManager inspections;
+//    InspectionManager inspections;
 
     public Restaurant() {
         this.trackingNumber = "";
@@ -18,64 +18,58 @@ public class Restaurant {
         this.facType = "";
         this.latitude = 0;
         this.longitude = 0;
-        //this.inspections = null;
+//        this.inspections = null;
     }
 
     public String getTrackingNumber() {
         return trackingNumber;
     }
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhysicalCity() {
+        return physicalCity;
+    }
+    public String getFacType() {
+        return facType;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+//    public InspectionManager getInspections() {
+//        return inspections;
+//    }
 
     public void setTrackingNumber(String trackingNumber) {
         this.trackingNumber = trackingNumber;
     }
-
-    public String getRestaurantName() {
-        return restaurantName;
-    }
-
     public void setRestaurantName(String restaurantName) {
         this.restaurantName = restaurantName;
     }
-
-    public String getAddress() {
-        return address;
-    }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
-    public String getPhysicalCity() {
-        return physicalCity;
-    }
-
     public void setPhysicalCity(String physicalCity) {
         this.physicalCity = physicalCity;
     }
-
-    public String getFacType() {
-        return facType;
-    }
-
     public void setFacType(String facType) {
         this.facType = facType;
     }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
+//    public void setInspections(InspectionManager inspections) {
+//        this.inspections = inspections;
+//    }
 
     @Override
     public String toString() {
